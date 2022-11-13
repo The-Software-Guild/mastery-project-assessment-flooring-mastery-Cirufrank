70084e5b7a521718befebea683ede7d5c5593086
Cirũ Franklin <cirufrank@me.com>
2022-11-13 13:40:13 -0800

    Complete intial implementation of ProductDaoFileImpl

--- FlooringServices/src/main/java/com/we/flooringservices/dao/ProductDaoFileImpl.java
+++ FlooringServices/src/main/java/com/we/flooringservices/dao/ProductDaoFileImpl.java
@@ -4,6 +4,22 @@
  */
 package com.we.flooringservices.dao;
 
+import com.we.flooringservices.model.Product;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
 /**
  *
  * @author Cirũ Franklin (she/they), Software Engineer
@@ -16,15 +32,122 @@ package com.we.flooringservices.dao;
  * file
  */
 
+@Component
+@Primary
 public class ProductDaoFileImpl implements ProductDao {
+    final private Map<Integer, Product> allProducts= new HashMap<>();
     final private static String DELIMITER = ",",
             PRODUCT_ID_HEADER = "productId",
             PRODUCT_TYPE_HEADER = "productType",
             SQUARE_FOOT_COST_HEADER = "costPerSquareFoot",
             SQUARE_FOOT_LABOR_COST_HEADER = "laborCostPerSquareFoot",
-            productsHeadersLine = PRODUCT_ID_HEADER + DELIMITER
+            productHeadersLine = PRODUCT_ID_HEADER + DELIMITER
             + PRODUCT_TYPE_HEADER + DELIMITER + SQUARE_FOOT_COST_HEADER
             + DELIMITER + SQUARE_FOOT_LABOR_COST_HEADER;
+    private String productsFileName;
     
+    public ProductDaoFileImpl() {
+        productsFileName = "Data/Products.txt";
+    }
     
+    public ProductDaoFileImpl(String productsFileName){
+        this.productsFileName = productsFileName;
+    }
+    
+    @Override
+    public List<Product> getAllProducts() {
+        loadProducts();
+        final List<Product> currentProducts =
+                new ArrayList<>(allProducts.values());
+        return currentProducts;
+    }
+    
+    @Override
+    public Product getProduct(int productId) {
+        loadProducts();
+        final Product productRetrieved = 
+                allProducts.get(productId);
+        return productRetrieved;
+    }
+    
+    @Override
+    public void addProduct(Product product) {
+        loadProducts();
+        allProducts.put(product.getProductId(), product);
+        writeProducts();
+    }
+    
+    private String marshallProduct(Product product) {
+        final String productAsText = DaoHelper
+                .createDelimiterSeparatedString(DELIMITER,
+                        Integer.toString(product.getProductId()),
+                        product.getProductType(), 
+                        product.getCostPerSquareFoot().toString(),
+                        product.getLaborCostPerSquareFoot().toString());
+        return productAsText;
+    }
+    
+    private Product unMarshallProduct(String productAsText) {
+        final String[] productTokens = productAsText.split(DELIMITER);
+        final int ID_INDEX = 0, PRODUCT_TYPE_INDEX = 1,
+                PRODUCT_SQUARE_FOOT_COST_INDEX = 2,
+                PRODUCT_LABOR_SQUARE_FOOT_COST_INDEX = 3,
+                productId = Integer.parseInt(productTokens[ID_INDEX]),
+                TOTAL_PRODUCTS = allProducts.size();
+        
+        final String productType = productTokens[PRODUCT_TYPE_INDEX];
+        final BigDecimal costPerSquareFoot = new BigDecimal(productTokens[
+                PRODUCT_SQUARE_FOOT_COST_INDEX]),
+                laborCostPerSquareFoot = new BigDecimal(productTokens[
+                        PRODUCT_LABOR_SQUARE_FOOT_COST_INDEX]);
+        
+        Product product = new Product(productId, productType,
+        costPerSquareFoot, laborCostPerSquareFoot, TOTAL_PRODUCTS);
+        return product;
+        
+    }
+    
+    private void loadProducts() {
+        try {
+            Scanner scanner =
+                    new Scanner( 
+                        new BufferedReader(
+                            new FileReader(productsFileName)));
+            //This skips the header line from being converted to
+            //a Product thus causing an error
+            scanner.nextLine();
+            while (scanner.hasNextLine()) {
+                final String productAsText = scanner.nextLine();
+                final Product currentProduct =
+                        unMarshallProduct(productAsText);
+                allProducts.put(currentProduct.getProductId(), currentProduct);
+            }
+            scanner.close();
+        } catch(FileNotFoundException error) {
+            System.out.println("-_- Unable to read"
+                    + "products from file");
+        }
+    }
+    
+    private void writeProducts() {
+        final ArrayList<Product> currentProducts =
+                new ArrayList<>(allProducts.values());
+        try {
+            PrintWriter output = 
+                    new PrintWriter(
+                        new FileWriter(productsFileName));
+            //Write the headers line to the file before writing the 
+            //products
+            output.println(productHeadersLine);
+            for (Product currentProduct: currentProducts) {
+                final String productAsText = marshallProduct(currentProduct);
+                output.println(productAsText);
+                output.flush();
+            }
+            output.close();
+        } catch(IOException error) {
+            System.out.println("-_- Could not write "
+                    + "products to file");
+        }
+    }
 }
